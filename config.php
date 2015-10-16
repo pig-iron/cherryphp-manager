3ef05b1723a017dd8673142fc849f27a255cf26c
Einstein.F <icewoo2@hotmail.com>
2015-10-16 11:16:08 +0800

    config.php

--- config.php
+++ config.php
@@ -11,15 +11,24 @@ return array(
     ),
     'redis'=>array(
         '0'=>array(
-			'cachehost'=>'10.5.121.124','cacheport'=>'12000',
+			'host'=>'10.225.2.48',
+            'port'=>'6359',
 		),
     ),
     'zookeeper'=>array(
         '0'=>array(
-			'ZOOKEEPER_HOST' => '10.235.2.211:2181,10.235.2.211:2182,10.235.2.211:2183',
+			'ZOOKEEPER_HOST' => '10.215.2.211:221,10.215.2.211:222,10.215.2.211:223',
 			'ZK_SERVER_PATH' => '/westM/kukuhero/server',
 			'ZK_MAIL_PATH'   => '/westM/kukuhero/mail',
 		),
+    ),
+    'aws'=>array(
+        '0'=>array(
+            'key'    => 'aaaaaaaaaaaa',
+            'secret' => 'aaaaaaaaaaaaaaaaaaaaaaa',
+            'region' => 'apasdsoutheastasdasdasd',
+            'bucket' =>'apfasfasf',
+        ),
     ),
 	'define'=>array(
 		'APP_TPL'     =>'view',//必须 定义模板目录
@@ -27,7 +36,7 @@ return array(
 		'APP_PORT'    =>'8008',//必须 定义APP端口号
 		'ERROR404'    =>'view/html/404.html',//必须 定义404错误页地址
 		'PAGE'        =>10,
-		'GMPORT'      =>'GMPort',
+		'GMPORT'      =>'GsssPort',
 		'SERVICE_IP'  =>'service_ip',
 	),
     'charset'=>'utf8',//必须 定义字符集
