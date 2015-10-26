04064e51b6daf5104704206c3a29ccf8087ecd9c
Einstein.F <icewoo2@hotmail.com>
2015-10-26 09:53:12 +0800

    config.php

--- config.php
+++ config.php
@@ -2,42 +2,42 @@
 return array(
     'mysql'=>array(
         '0'=>array(
-            'dblocal' =>  'localhost',
-            'dbport'  =>  '3306',
-            'dbuser'  =>  'root',
-            'dbpass'  =>  '123',
-            'dbname'  =>  'rogmgr',
+            'dblocal' =>  'XXXX',
+            'dbport'  =>  'XXXX',
+            'dbuser'  =>  'XXXX',
+            'dbpass'  =>  'XXXX',
+            'dbname'  =>  'XXXX',
         ),
     ),
     'redis'=>array(
         '0'=>array(
-			'host'=>'10.225.2.48',
-            'port'=>'6359',
-		),
+		'host'=>'XXXX',
+        	'port'=>'XXXX',
+	),
     ),
     'zookeeper'=>array(
         '0'=>array(
-			'ZOOKEEPER_HOST' => '10.215.2.211:221,10.215.2.211:222,10.215.2.211:223',
-			'ZK_SERVER_PATH' => '/westM/kukuhero/server',
-			'ZK_MAIL_PATH'   => '/westM/kukuhero/mail',
+			'ZOOKEEPER_HOST' => 'XXXX:XXX,XXXX:XXX,XXXX:XXX',
+			'ZK_SERVER_PATH' => '/XXXX/XXXX/XXXX',
+			'ZK_MAIL_PATH'   => '/XXXX/XXXX/XXXX',
 		),
     ),
     'aws'=>array(
         '0'=>array(
-            'key'    => 'aaaaaaaaaaaa',
-            'secret' => 'aaaaaaaaaaaaaaaaaaaaaaa',
-            'region' => 'apasdsoutheastasdasdasd',
-            'bucket' =>'apfasfasf',
+            'key'    => 'XXXX',
+            'secret' => 'XXXX',
+            'region' => 'XXXX',
+            'bucket' => 'XXXX',
         ),
     ),
 	'define'=>array(
 		'APP_TPL'     =>'view',//必须 定义模板目录
-		'APP_PATH'    =>'/www/rogmgr/',//必须 定义APP根目录
-		'APP_PORT'    =>'8008',//必须 定义APP端口号
+		'APP_PATH'    =>'/XXXX/XXXX/',//必须 定义APP根目录
+		'APP_PORT'    =>'XXXX',//必须 定义APP端口号
 		'ERROR404'    =>'view/html/404.html',//必须 定义404错误页地址
 		'PAGE'        =>10,
-		'GMPORT'      =>'GsssPort',
-		'SERVICE_IP'  =>'service_ip',
+		'GMPORT'      =>'XXXX',
+		'SERVICE_IP'  =>'XXXX',
 	),
     'charset'=>'utf8',//必须 定义字符集
 );
