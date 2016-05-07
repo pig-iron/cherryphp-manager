dbfe0dea8ce9433c5c77ab8a9342e5502a99e47b
rryqszq4 <279664262@163.com>
2016-05-07 15:05:18 +0800

    [mcp] udpate main.php undefine 'r_t'

--- mcp/main.php
+++ mcp/main.php
@@ -8,20 +8,20 @@ class main extends \cherryphp\CherryView
 	}
 	public function index($params)
 	{
-		if ($_COOKIE['r_t']){
-            $rules=\module\mauthrules::authtoken();
-            if ($rules){
-                $r_t=\module\mauthrules::preventinjection($_COOKIE['r_t']);
-                $this->assign("agent",$_SESSION[$r_t]);
-                $this->assign("rules",explode(",",$rules));
-                $rules_kv=\module\mauthorization::get_rules_kv();
-                $this->assign("rules_kv",$rules_kv);
-                $this->render("mcp/main");
-            }else{
-                header("Location: /login");
-            }
+		if (isset($_COOKIE['r_t']) && $_COOKIE['r_t']){
+			$rules=\module\mauthrules::authtoken();
+			if ($rules){
+				$r_t=\module\mauthrules::preventinjection($_COOKIE['r_t']);
+				$this->assign("agent",$_SESSION[$r_t]);
+				$this->assign("rules",explode(",",$rules));
+				$rules_kv=\module\mauthorization::get_rules_kv();
+				$this->assign("rules_kv",$rules_kv);
+				$this->render("mcp/main");
+			}else{
+				header("Location: /login");
+			}
 		}else{
-            header("Location: /login");
+            		header("Location: /login");
         }
 	}
 
