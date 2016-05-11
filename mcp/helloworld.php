0ba91b28580a431e5d9392edc6003c38216698c4
icewoo2@hotmail.com <icewoo2@hotmail.com>
2016-05-12 04:56:59 +0800

    config.php

---
+++ mcp/helloworld.php
@@ -0,0 +1,15 @@
+<?php
+namespace mcp;
+class helloworld extends \cherryphp\CherryView
+{
+   
+    public function __construct()
+    {
+        
+    }
+
+    public function index($params)
+    {
+        echo "helloworld";
+    }
+}
